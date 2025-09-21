648e7655bfbf560002349fb06bb591068e412dad
uchiumia2to <uchiumia2to@gmail.com>
2025-09-21 09:01:24 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
   },
 }
 
